0aa7c1dac41616bfe8d587faaf2c19d12b02558b
Dhairya329 <dhairyagupta329@gmail.com>
2025-06-21 00:39:02 +0530

    Day 1- Palindrome Number

---
+++ Leetcode9.java
@@ -0,0 +1,42 @@
+/* Problem -2
+ * LeetCode Problem #9: Palindrome Number
+ * https://leetcode.com/problems/palindrome-number/description/
+ * Difficulty: Easy
+ * 
+ * Approach: Math 
+ * Author: Dhairya Gupta 
+ * 
+ */
+ 
+// Time complexity: O(log n)
+// Space complexity: O(1)
+
+ class Solution {
+
+    // Method to check if a given integer is a palindrome
+    static boolean isPalindrome(int num) {
+
+        // Negative numbers are not palindromes
+        if (num < 0) {
+            return false;
+        }
+
+        int copy = num;     // Copy of original number
+        int sum = 0;        // To hold the reversed number
+
+        // Reverse the number
+        while (num != 0) {
+            int mod = num % 10;         // Get the last digit
+            sum = sum * 10 + mod;       // Append the digit to the reversed number
+            num /= 10;                  // Remove the last digit
+        }
+
+        // Compare reversed number with original
+        return copy == sum;
+    }
+
+    public static void main(String[] args) {
+        int num = 1331;
+        System.out.println(isPalindrome(num));  
+    }
+}
