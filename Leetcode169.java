5c04062fa0f9609f994a77c667c4c867c8a6e04e
Dhairya329 <dhairyagupta329@gmail.com>
2025-06-26 18:37:42 +0530

    Day 8- Majority Element

---
+++ Leetcode169.java
@@ -0,0 +1,38 @@
+/* Problem -14
+ * LeetCode Problem #169: Majority Element
+ * https://leetcode.com/problems/majority-element/description/
+ * Difficulty: Easy
+ * 
+ * Author: Dhairya Gupta 
+ * 
+ */
+ 
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+// Boyer-Moore Majority Voting Algorithm
+
+class Solution {
+    static int majorityElement(int[] nums) {
+        int count = 0;              // tracks balance of votes
+        int candidate = 0;          // stores current majority guess
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {       // reset candidate when votes are zero
+                candidate = nums[i];
+            }
+            if (nums[i] == candidate) {
+                count++;           
+            } else {
+                count--;         
+            }
+        }
+        return candidate;           // guaranteed majority, return it
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 3, 2, 3 };
+        System.out.println(majorityElement(arr)); 
+    }
+}
