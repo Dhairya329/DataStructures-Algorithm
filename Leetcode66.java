20c2d811c3c69f4174d0bd96a5611fb6271140dc
Dhairya329 <dhairyagupta329@gmail.com>
2025-07-02 18:16:37 +0530

    Day 14- Plus One

---
+++ Leetcode66.java
@@ -0,0 +1,38 @@
+/* Problem -20
+ * LeetCode Problem #66: Plus One
+ * https://leetcode.com/problems/plus-one/description/
+ * Difficulty: Easy
+ * 
+ * Author: Dhairya Gupta 
+ * 
+ */
+ 
+// Time complexity: O(n)
+// Space complexity: O(n) for worst case
+
+class Solution {
+
+    static int[] plusOne(int[] arr) {
+        int n = arr.length;
+
+        // Traverse from the end to handle carry
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] < 9) {
+                arr[i]++; // return if no carry
+                return arr;
+            }
+            arr[i] = 0; // set every digit to 0 if digit is 9 
+        }
+
+        // if all digits were 9, create a new array with 1 extra space
+        int[] new_array = new int[n + 1];
+        new_array[0] = 1; // first digit 1 then all 0
+        return new_array;
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = { 9 };
+        plusOne(arr);
+    }
+}
